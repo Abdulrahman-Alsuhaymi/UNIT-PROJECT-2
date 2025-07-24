bd43833c32e403e769b88c00ca3cc9ae1fa09b0a
Abdulrahman <a3.alsuhaymi@gmail.com>
2025-07-24 13:23:39 +0300

    Finished the backend of the project

--- ArtSpace/accounts/admin.py
+++ ArtSpace/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ArtistProfile
 
 # Register your models here.
+class ArtistProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio', 'profile_pic')
+    list_filter = ('user__is_active',)
+    search_fields = ('user__username', 'user__email', 'bio')
+
+admin.site.register(ArtistProfile, ArtistProfileAdmin)
